0b20d339fb81980389e7b41553480091b8c43b10
Slava <slavapv85@gmail.com>
2019-05-30 15:58:59 +0300

    base Url

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,7 @@ try {
  */
 
 window.axios = require('axios');
-
+window.axios.defaults.baseURL = window.location.origin;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
