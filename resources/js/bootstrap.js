25310dc11a1b8597c96f906f1cea29754ca9824c
Slava <slavapv85@gmail.com>
2019-05-30 16:12:54 +0300

    base url

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,7 @@ try {
  */
 
 window.axios = require('axios');
-window.axios.defaults.baseURL = window.location.origin;
+window.axios.defaults.baseURL = 'https://myfullproject.herokuapp.com';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
